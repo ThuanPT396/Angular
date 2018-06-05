15102ed7f21dafba06f58a66ddc3a37eb6c9e643
Nguyen The Phuong <thephuongcqt@gmail.com>
2018-06-05 15:48:16 +0700

    fix bugs

--- src/app/list-user/user-list.component.ts
+++ src/app/list-user/user-list.component.ts
@@ -11,20 +11,10 @@ import { HttpClient } from '@angular/common/http';
   providers: [UserService]
 })
 export class UserListComponent implements OnInit {
-  // name = "";
-  // password = "";
-  // phoneNumber = 0;
-  // role=0;
+  dtOptions: DataTables.Settings = {};
   message: string;
   avtived = "false";
   users: User[];
-
-  // users: User[] = [
-  //   new User('thuan@gmail.com', 'password', 1234567893, 'admin', 'true'),
-  //   new User('kiet@gmail.com', 'passkiet', 6376182736, 'admin', 'false'),
-  //   new User('phuong@gmail.com', 'passphuong', 9898988923, 'admin', 'true'),
-  //   new User('duy@gmail.com', 'passduyd', 4123213444, 'admin', 'true'),
-  // ];
 
   constructor(private userService: UserService) { }
 
@@ -33,40 +23,17 @@ export class UserListComponent implements OnInit {
       .getUsers()
       .subscribe((response) => {
         this.users = response.value;
+        var ajaxObj = {
+          "data": this.users
+        };
         console.log(this.users);
-        // var tmp = JSON.parse(JSON.stringify(response));
-        // this.users =[];
-        // if (tmp.status == true) {
-        //   // this.users = tmp.value;
-        //   for (var i in tmp.value) {
-        //     var user = tmp.value[i];
-        //     var result = new User(user.username, "1234567", user.phoneNumber, user.role, user.isActive);
-        //     this.users.push(result);
-        //     console.log(user);
-        //   }
-        // } else {
-        //   console.log(tmp.error);
-        // }
+
+        this.dtOptions = {
+          pagingType: 'full_numbers',
+          ajax: ajaxObj
+        };
+
       });
   }
 }
-
-  // onUserAdded($event) {
-  //   this.users.push($event);
-  // }
-  // onUserDelete(userName: string) {
-  //   const index = this.users.findIndex(user => user.userName === userName);
-  //   this.users[index].isActive = this.avtived;
-  //   this.users.splice(index, 1);
-  // }
-  // onUserPopup(userName: string, phoneNumber:number){
-  //  const index= this.patients.findIndex(patient =>patient.name === name);
-  //  this.name = name;
-  //  this.phoneNumber = phoneNumber;
-  // }
-  // onSubmitUpdate(id:number){
-  //    const index= this.patients.findIndex(patient =>patient.id === id);
-  //    this.patients[index].name=this.name;
-  //    this.patients[index].phoneNumber=this.phoneNumber;
-  // }
 

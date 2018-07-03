ca9f141663e88b293b60b98a24a18944bc5c1228
Nguyen The Phuong <thephuongcqt@gmail.com>
2018-07-03 17:18:05 +0700

    thuận gà

--- src/app/list-chart/chart-date/chart-date.component.ts
+++ src/app/list-chart/chart-date/chart-date.component.ts
@@ -1,29 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { AppointmentService } from '../../service/appointment.service';
 import { Chart } from '../../model/chart.model';
+import * as GGChart from "../../../chart.js";
 
 @Component({
   selector: 'app-chart-date',
   templateUrl: './chart-date.component.html',
   styleUrls: ['./chart-date.component.css'],
-  providers:[AppointmentService]
+  providers: [AppointmentService]
 })
 export class ChartDateComponent implements OnInit {
 
- 
+
   username = localStorage.getItem('username')
   constructor(private appointmentService: AppointmentService) { }
 
   ngOnInit() {
+    // GGChart.dm(["abc", "cds", "ds"]);
     this.appointmentService
-    .postChartByDate(this.username,"2017-06-06","2019-07-07")
-    .subscribe((response) => {
-      var tmp = JSON.parse(JSON.stringify(response));
-      for (var i in tmp.value) {
-        var app = tmp.value[i];
-        var result = new Chart(app.total,app.present,app.date);
-        console.log(result)
-      }
-    })
-  }
+      .postChartByDate(this.username, "2017-06-06", "2019-07-07")
+      .subscribe((response) => {
+        var tmp = JSON.parse(JSON.stringify(response));
+        var data = [];
+        for (var i in tmp.value) {
+          var app = tmp.value[i];
+          var result = new Chart(app.total, app.present, app.date);
+          data.push(result);
+        }
+        GGChart.drawChartForDate(data);
+      });
+    }
 }

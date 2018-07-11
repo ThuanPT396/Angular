0861093d8a6db05f34402209b39421b43dd24f65
THUANPTSE62063 <thuanpt159@gmail.com>
2018-07-11 22:26:02 +0700

    Revert "done benh an"
    This reverts commit 95b8c18ecafe6bd075108e81c7ade7a16285dd87.

--- src/app/model/prescription.model.ts
+++
@@ -1,9 +0,0 @@
-export class Prescription {
-    constructor(
-        public appointmentID: string,
-        public reminding: number,
-        public description: string,
-        public medicines: any,
-        public disease: any
-    ) { }
-}

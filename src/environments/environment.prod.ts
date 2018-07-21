d02753e40baba54427278901cd5bfc387d51e131
THUANPTSE62063 <thuanpt159@gmail.com>
2018-07-21 15:36:54 +0700

    fix heroku enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDfHYz3IsP_vaUIHPtsJIoAQP2tuXt4Y3o",
+    authDomain: "callcenter2-79faf.firebaseapp.com",
+    databaseURL: "https://callcenter2-79faf.firebaseio.com",
+    projectId: "callcenter2-79faf",
+    storageBucket: "callcenter2-79faf.appspot.com",
+    messagingSenderId: "437377487737"
+  }
 };

12a08a2b92cf77a629c7742fa5f83608eb4825c1
Nguyen The Phuong <thephuongcqt@gmail.com>
2018-07-21 08:45:59 +0700

    edit observer notification

--- src/app/components/appheader/appheader.component.ts
+++ src/app/components/appheader/appheader.component.ts
@@ -3,6 +3,9 @@ import { Router } from '@angular/router';
 import { UserService } from '../../service/user.service';
 import { ToasterService } from '../../service/toast/toaster.service';
 import { MyNotification } from '../../model/notification.model';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-appheader',
@@ -12,65 +15,71 @@ import { MyNotification } from '../../model/notification.model';
 export class AppheaderComponent implements OnInit {
   username = ""
   fullName = ""
-  phoneNumber=""
-  hidden=false;
+  phoneNumber = ""
+  hidden = false;
   currentpw = ""
   newpw = ""
   confirmpw = ""
-  textValidPW="";
-  textConfirmPW="";
-  notifications: MyNotification[] = [];
-  constructor(private router: Router, private userService: UserService, private toastService: ToasterService) { }
-  
-  updateNotification(notis: MyNotification[]){
-    this.notifications.pop();
-    for(var index in notis){
-      var item = notis[index];
-      this.notifications.push(item);
-      console.log(item);
-    }    
+  textValidPW = "";
+  textConfirmPW = "";
+  private notifications: AngularFirestoreCollection<MyNotification>;
+  private notiOnView: MyNotification[] = [];
+  constructor(private router: Router, private userService: UserService, private toastService: ToasterService, private db: AngularFirestore) { }
+
+  listenNotification() {
+    this.notiOnView.pop();
+    var username = localStorage.getItem("username");
+    this.notifications = this.db.collection("callcenter/" + username + "/notifications");
+    this.notifications.valueChanges().subscribe(collection => {
+      for (var index in collection) {
+        var item = collection[index];
+        this.notiOnView.push(item);
+      }
+    });
   }
+
   ngOnInit() {
+    this.listenNotification();
     var result = this.userService.getUserClaims();
-    if (localStorage.getItem('role')!='1') {
+    if (localStorage.getItem('role') != '1') {
       this.fullName = result.fullName
-      this.hidden=true;
-    }else{
+      this.hidden = true;
+    } else {
       this.fullName = result.clinicName
-      this.hidden=false;
+      this.hidden = false;
     }
     this.username = result.username
-    this.phoneNumber=result.phoneNumber
+    this.phoneNumber = result.phoneNumber
   }
   logout() {
-    this.userService.postUnsubscribe(localStorage.getItem('firebaseToken'),localStorage.getItem('username'))
-    .subscribe(res => {
-      console.log(res)
-    })
+    this.userService.postUnsubscribe(localStorage.getItem('firebaseToken'), localStorage.getItem('username'))
+      .subscribe(res => {
+        console.log(res)
+      })
     localStorage.removeItem('clinicName');
     localStorage.removeItem('username');
     localStorage.removeItem('fullName');
     localStorage.removeItem('role');
     localStorage.removeItem('firebaseToken')
-    
+
   }
-  checkPassword(username:string, password:string){
-      this.userService
-      .userCheckPassword(username,password)
+  checkPassword(username: string, password: string) {
+    this.userService
+      .userCheckPassword(username, password)
       .subscribe((response) => {
         var tmp = JSON.parse(JSON.stringify(response));
         if (tmp.status == true) {
-          this.textValidPW="";
-        }else{
-          this.textValidPW="Password is invalid";
+          this.textValidPW = "";
+        } else {
+          this.textValidPW = "Password is invalid";
         }
       })
   }
-  ConfirmPW(newpw,confirmpw){
-    if (newpw!=confirmpw) {
-      this.textConfirmPW="Confirm Password isn't correct";
-    }else{
-      this.textConfirmPW=""
+  ConfirmPW(newpw, confirmpw) {
+    if (newpw != confirmpw) {
+      this.textConfirmPW = "Confirm Password isn't correct";
+    } else {
+      this.textConfirmPW = ""
     }
   }
   onChangePassword(username: string, password: string, newPassword: string) {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../service/user.service';
-import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { ToasterService } from '../service/toast/toaster.service';
 import { MyNotification } from '../model/notification.model';
 import { AppheaderComponent } from '../components/appheader/appheader.component';
@@ -15,28 +12,16 @@ import { AppheaderComponent } from '../components/appheader/appheader.component'
   providers: [AppheaderComponent]
 })
 export class HomeComponent implements OnInit {
-  role = 0;  
-  private notifications: AngularFirestoreCollection<MyNotification>;
+  role = 0;
   constructor(
     private userService: UserService,
-    private db: AngularFirestore,
-    private toastService: ToasterService, 
+    private toastService: ToasterService,
     private appheader: AppheaderComponent) {
-    var username = localStorage.getItem("username");
-    this.notifications = db.collection("callcenter/" + username + "/notifications");
   }
 
   ngOnInit() {
     var result = this.userService.getUserClaims();
     this.role = parseInt(result.role);
-
-
-    this.notifications.valueChanges().subscribe(collection => {
-      for (var index in collection) {
-        var item = collection[index];
-      }        
-      this.appheader.updateNotification(collection);
-    });
 
     // this.notifications.stateChanges(['added']).pipe(
     //   map(actions => actions.map(a => {

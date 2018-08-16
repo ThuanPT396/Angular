45673dc092a7e04b90abee33f1cc19ead78ec11c
Nguyen The Phuong <thephuongcqt@gmail.com>
2018-08-16 10:03:46 +0700

    change endpoint

--- src/app/Const.ts
+++ src/app/Const.ts
@@ -1,5 +1,6 @@
 export class Final {
   // public static API_ENDPOINT='http://localhost:8080';
   // public static API_ENDPOINT='http://callcenterapi.herokuapp.com:8080';
-  public static API_ENDPOINT='http://27.74.245.84:8080';
+  // public static API_ENDPOINT='http://27.74.245.84:8080';
+  public static API_ENDPOINT='http://115.78.4.37:8080';
 }

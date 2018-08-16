6c7323e9081137480fc7024807cb40dd98d62138
Nguyen The Phuong <thephuongcqt@gmail.com>
2018-08-16 10:08:34 +0700

    aslkd

--- src/app/Const.ts
+++ src/app/Const.ts
@@ -1,7 +1,7 @@
 export class Final {
   // public static API_ENDPOINT='http://localhost:8080';
   // public static API_ENDPOINT='http://callcenterapi.herokuapp.com:8080';
-  public static API_ENDPOINT='http://27.74.245.84:8080';
+  // public static API_ENDPOINT='http://27.74.245.84:8080';
   // public static API_ENDPOINT='https://apicallcenter.herokuapp.com';
-  // public static API_ENDPOINT='http://115.78.4.37:8080';
+  public static API_ENDPOINT='http://115.78.4.37:8080';
 }
